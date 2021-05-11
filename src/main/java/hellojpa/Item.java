33162f30327f4ab71ae644efa8af688ff6435f47
Huhsway <seunghoe9542@naver.com>
2021-05-11 10:19:41 +0900

    단일 테이블 전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,7 +3,7 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn // 엔티티 명이 들어감
 public class Item {
 
