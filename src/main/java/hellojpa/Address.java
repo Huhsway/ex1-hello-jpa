572591ca2f8c1048e4a4899e31c2b6fb9e2f5c18
Huhsway <seunghoe9542@naver.com>
2021-05-14 18:38:08 +0900

    값 타입의 비교

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -3,6 +3,7 @@ package hellojpa;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -34,4 +35,28 @@ public class Address {
         return zipcode;
     }
 
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
